462f73b6ab261e70ff50abf572f4f62110cdef3a
EunJung803 <aimee3322@gmail.com>
2022-07-18 13:44:55 +0900

    setting + 클래스가_존재한다 Test

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
+public class AppTest {
+}
+
+class HashMapTest {
+    @Test
+    void 클래스가_존재한다() {
+        HashMap map = new HashMap();
+    }
+}
